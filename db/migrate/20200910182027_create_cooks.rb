164924052f5fbc60e7b678211a2b40b79e3d2585
Ærika <aerikajasso@gmail.com>
2020-09-10 11:29:00 -0700

    Create Cook and Recipe Migrations Create Cook and Recipe Models

---
+++ db/migrate/20200910182027_create_cooks.rb
@@ -0,0 +1,9 @@
+class CreateCooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cooks do |t|
+      t.string  :cook_name
+      t.string  :email
+      t.timestamps
+    end
+  end
+end
